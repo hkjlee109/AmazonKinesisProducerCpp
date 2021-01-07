08fd236ce6781eb32a2fac7aca647aceb0e9e081
kwoolytech <kwooly.tech@gmail.com>
2021-01-07 21:45:44 +0100

    Almost work.

--- build_target.sh
+++ build_target.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-TOOLCHAIN_PATH=$(pwd)/Toolchains/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin
+TOOLCHAIN_PATH=`pwd`/Toolchains/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin
 
 PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/lib/pkgconfig \
-CC=$(TOOLCHAIN_PATH)/arm-linux-gnueabihf-gcc \
-CXX=$(TOOLCHAIN_PATH)/arm-linux-gnueabihf-c++ \
-LD=$(TOOLCHAIN_PATH)/arm-linux-gnueabihf-ld \
+CC=$TOOLCHAIN_PATH/arm-linux-gnueabihf-gcc \
+CXX=$TOOLCHAIN_PATH/arm-linux-gnueabihf-c++ \
+LD=$TOOLCHAIN_PATH/arm-linux-gnueabihf-ld \
 CFLAGS="-fPIC" \
 CXXFLAGS="-fPIC" \
-cmake -DBUILD_OPENSSL_PLATFORM=linux-generic32 -S . -B build && cmake --build build
+cmake -DBUILD_OPENSSL_PLATFORM=linux-generic32 -S . -B build/armhf && cmake --build build/armhf

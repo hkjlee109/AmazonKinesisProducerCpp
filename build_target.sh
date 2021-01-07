0f02b6f38e6258cdf3687e4f7bc7d0d7c879f4e3
kwoolytech <kwooly.tech@gmail.com>
2021-01-07 21:37:21 +0100

    Almost work.

--- build_target.sh
+++ build_target.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
-PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig:/usr/lib/pkgconfig \
-CC=~/git/AmazonKinesisProducerCpp/Toolchains/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc \
+TOOLCHAIN_PATH=$(pwd)/Toolchains/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin
+
+PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/lib/pkgconfig \
+CC=$(TOOLCHAIN_PATH)/arm-linux-gnueabihf-gcc \
+CXX=$(TOOLCHAIN_PATH)/arm-linux-gnueabihf-c++ \
+LD=$(TOOLCHAIN_PATH)/arm-linux-gnueabihf-ld \
 CFLAGS="-fPIC" \
-CXX=~/git/AmazonKinesisProducerCpp/Toolchains/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-c++ \
 CXXFLAGS="-fPIC" \
-LD=~/git/AmazonKinesisProducerCpp/Toolchains/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-ld \
 cmake -DBUILD_OPENSSL_PLATFORM=linux-generic32 -S . -B build && cmake --build build
